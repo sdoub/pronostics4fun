51a348e2a30d84faf88b9c9ac34ef75edf289d75
sebastien.dubuc@gmail.com <sebastien.dubuc@gmail.com@7485a9c5-63d3-be51-62df-b1c5087495a6>
2011-11-12 09:52:12 +0000

    Correction du bug lors de la sauvegarde des news (Probleme d'encoding)

--- save.news.php
+++ save.news.php
@@ -5,7 +5,8 @@ require_once("begin.file.php");
 $newsId = explode('.',$_POST["element_id"]);
 $newsKey = $newsId[1];
 
-$newsInfo = mysql_real_escape_string(__encode(utf8_decode($_POST["update_value"])));
+//$newsInfo = mysql_real_escape_string(__encode(utf8_decode($_POST["update_value"])));
+$newsInfo = utf8_decode($_POST["update_value"]);
 if ($newsKey=="newKey") {
   $query = "INSERT INTO news (CompetitionKey, Information, InfoType) VALUE (".COMPETITION.",'".$newsInfo."', 4)";
 }
@@ -15,12 +16,11 @@ else {
 
 $_databaseObject -> queryPerf ($query , "Update News");
 
-$newsKey = $_databaseObject -> insert_id();
+if ($newsKey=="newKey") {
+  $newsKey = $_databaseObject -> insert_id();
+}
 
-print($_POST["update_value"]);
-
-//writePerfInfo();
-
+print(stripcslashes (utf8_encode($newsInfo)));
 
 require_once("end.file.php");
 
