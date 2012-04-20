192933d04ba7630431da1ae16e8ae231fc6e6182
sebastien.dubuc@gmail.com <sebastien.dubuc@gmail.com@7485a9c5-63d3-be51-62df-b1c5087495a6>
2012-04-20 12:44:32 +0000

    NEW: Amélioration du système de récupération des données à partir d'un site web externe

---
+++ branches/Version 1.1/refresh.match.php
@@ -0,0 +1,39 @@
+#!/usr/local/bin/php
+<?php
+include_once(dirname(__FILE__)."/begin.file.php");
+include_once(dirname(__FILE__). "/lib/match.php");
+include_once(dirname(__FILE__). "/lib/ranking.php");
+include_once(dirname(__FILE__). "/lib/score.php");
+include_once(dirname(__FILE__). "/lib/p4fmailer.php");
+
+$_databaseObject->close();
+
+$_queries =array();
+$arr = array();
+if (isset($_POST["TeamHomeKey"])) {
+  $teamHomeKey = $_POST["TeamHomeKey"];
+  $teamAwayKey = $_POST["TeamAwayKey"];
+  $externalKey = $_POST["ExternalKey"];
+  $matchKey = $_POST["MatchKey"];
+  $isLive = $_POST["Live"];
+
+  $matchInfo = GetMatchInfo($teamHomeKey,$teamAwayKey,$externalKey,$matchKey,$isLive=="1");
+  foreach ($matchInfo["Queries"] as $query) {
+    $_queries[] =$query;
+  }
+  $matchInfo = GetMatchsLineupsInfo($teamHomeKey,$teamAwayKey,$externalKey,$matchKey,$isLive=="1",$matchInfo["HomeId"],$matchInfo["AwayId"]);
+  foreach ($matchInfo["Queries"] as $query) {
+    $_queries[] =$query;
+  }
+  $arr["Parameters"] = $_POST;
+  $arr["Status"] = "Success";
+} else
+{
+  $arr["Status"] = "Failed";
+}
+
+$arr["Queries"] = $_queries;
+
+writeJsonResponse($arr);
+include_once(dirname(__FILE__)."/end.file.php");
+?>
