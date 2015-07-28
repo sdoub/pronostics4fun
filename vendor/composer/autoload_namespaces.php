afbc0163e336db14fd37ec787cf77a0cdcadb3de
Sebastien Dubuc <sebastien.dubuc@gmail.com>
2015-07-28 08:00:52 -0400

    Added propel references

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
+    'Propel' => array($vendorDir . '/propel/propel/src'),
     'Monolog' => array($vendorDir . '/monolog/monolog/src'),
 );
