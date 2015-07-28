980b7d02e700a45edb93cd838f8b19721b321fb9
Sebastien Dubuc <sebastien.dubuc@gmail.com>
2015-07-28 07:58:58 -0400

    Removed propel

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,7 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Psr\\Log\\' => array($vendorDir . '/psr/log'),
-    'Propel' => array($vendorDir . '/propel/propel/src'),
     'Monolog' => array($vendorDir . '/monolog/monolog/src'),
 );
